73f2a060aacf18b0ef6838fcb8754e5af0201201
ishsgit <aishtahad@yahoo.com>
2024-10-03 19:58:01 -0400

    implemented input box and subquery

--- frontend/drone-query/src/app/user-input/user-input.component.ts
+++ frontend/drone-query/src/app/user-input/user-input.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, MatButtonModule, MatCardModule, FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.scss'
 })
 export class UserInputComponent {
-
+  userInput: string = '';
+  submitQuery(){
+    console.log(this.userInput);
+  }
 }
